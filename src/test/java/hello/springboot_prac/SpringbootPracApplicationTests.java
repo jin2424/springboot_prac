8023e8ee7159509a79fc864d178e62ca503ce8fd
KGJ24 <rhrhkdwls24@naver.com>
2022-09-17 16:15:56 +0900

    HelloController

---
+++ src/test/java/hello/springboot_prac/SpringbootPracApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springboot_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
